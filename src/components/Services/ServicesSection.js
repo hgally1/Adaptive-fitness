03bb6f6457c5bd27cd0515dfca0045f7abe09976
github username <github email address>
2020-12-03 11:41:52 -0600

    added firebase auth and database and restructured files

---
+++ src/components/Services/ServicesSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ServicesSection = (props) => {
+	
+	return (
+		<div className="d-flex justify-content-center">
+			<div className="features">
+				<div className="col-md-3 fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<div className="feature-wrap">
+						<div className="icon">
+							<img src={props.img} className={props.img}></img>
+						</div>
+						<div>
+						<h2>{props.title}</h2>
+						<p>{props.desc}</p>
+						</div>
+					</div>	
+						<div className="btns">
+							<a className="btn btn-primary btn-top" href={props.href}>Learn More</a>
+						</div>
+					
+				</div>
+			</div>
+		</div>
+	)
+}
+
+ServicesSection.defaultProps = {
+	img: 'Image Here',
+	title: 'Title Here',
+	desc: 'Desc Here',
+	href: 'Href Here'
+}
+
+export default ServicesSection;
